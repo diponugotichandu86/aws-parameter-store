666bb785b079da6ffe94cb928c74fd9e70e9ef64
CHANDRASEKHAR <diponugotichandu@gmail.com>
2023-11-06 14:08:59 +0530

    roboshop project - Terraform

--- params.tf
+++ params.tf
@@ -7,10 +7,10 @@ resource "aws_ssm_parameter" "param" {
 
 variable "params" {
   default = [
-    {name="roboshop.dev.frontend.catalogue_url",value="https://catalogue-dev.devops86.store",type="String"},
-    {name="roboshop.dev.frontend.user_url",value="https://user-dev.devops86.store",type="String"},
-    {name="roboshop.dev.frontend.cart_url",value="https://cart-dev.devops86.store",type="String"},
-    {name="roboshop.dev.frontend.payment_url",value="https://payment-dev.devops86.store",type="String"},
-    {name="roboshop.dev.frontend.shipping_url",value="https://shipping-dev.devops86.store",type="String"},
+    {name="roboshop.dev.frontend.catalogue_url",value="https://catalogue-dev.devops86.store:8080/",type="String"},
+    {name="roboshop.dev.frontend.user_url",value="https://user-dev.devops86.store:8080/",type="String"},
+    {name="roboshop.dev.frontend.cart_url",value="https://cart-dev.devops86.store:8080/",type="String"},
+    {name="roboshop.dev.frontend.payment_url",value="https://payment-dev.devops86.store:8080/",type="String"},
+    {name="roboshop.dev.frontend.shipping_url",value="https://shipping-dev.devops86.store:8080/",type="String"},
   ]
 }
