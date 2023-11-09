20f54faf6a7be0ca54f997b494780747158426bf
CHANDRASEKHAR <diponugotichandu@gmail.com>
2023-11-09 12:50:51 +0530

    roboshop project - Terraform

--- params.tf
+++ params.tf
@@ -1,4 +1,4 @@
-resource "aws_ssm_parameter" "param" {
+resource "aws_ssm_parameter" "params" {
   count = length(var.params)
   name  = var.params[count.index].name
   type  = var.params[count.index].type
@@ -7,15 +7,19 @@ resource "aws_ssm_parameter" "param" {
 
 variable "params" {
   default = [
-    {name="roboshop.dev.frontend.catalogue_url",value="https://catalogue-dev.devops86.store:8080/",type="String"},
-    {name="roboshop.dev.frontend.user_url",value="https://user-dev.devops86.store:8080/",type="String"},
-    {name="roboshop.dev.frontend.cart_url",value="https://cart-dev.devops86.store:8080/",type="String"},
-    {name="roboshop.dev.frontend.payment_url",value="https://payment-dev.devops86.store:8080/",type="String"},
+    { name = "roboshop.dev.frontend.catalogue_url", value = "http://catalogue-dev.rdevopsb73.online/", type = "String" },
+    { name = "roboshop.dev.frontend.user_url", value = "http://user-dev.rdevopsb73.online/", type = "String" },
+    { name = "roboshop.dev.frontend.cart_url", value = "http://cart-dev.rdevopsb73.online/", type = "String" },
+    { name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.rdevopsb73.online/", type = "String" },
+    { name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.rdevopsb73.online/", type = "String" },
+
     {name="roboshop.dev.cart.redis_host",value="redis-dev.devops86.store",type="String"},
     {name="roboshop.dev.cart.catalogue_host",value="catalogue-dev.devops86.store",type="String"},
     {name="roboshop.dev.cart.catalogue_port",value="8080",type="String"},
+
     {name="roboshop.dev.catalogue.mongo",value="true",type="String"},
     {name="roboshop.dev.catalogue.mongo_url",value="mongodb://mongodb-dev.devops86.store:27017/catalogue",type="String"},
+
     {name="roboshop.dev.payment.cart_host",value="cart-dev.devops86.store",type="String"},
     {name="roboshop.dev.payment.cart_port",value="8080",type="String"},
     {name="roboshop.dev.payment.user_host",value="user-dev.devops86.store",type="String"},
@@ -23,8 +27,10 @@ variable "params" {
     {name="roboshop.dev.payment.amqp_host",value="rabbitmq-dev.devops86.store",type="String"},
     {name="roboshop.dev.payment.amqp_user",value="roboshop",type="String"},
     {name="roboshop.dev.payment.amqp_pass",value="roboshop123",type="SecureString"},
+
     {name="roboshop.dev.shipping.cart_endpoint",value="cart-dev.devops86.store:8080",type="String"},
     {name="roboshop.dev.shipping.db_host",value="mysql-dev.devops86.store",type="String"},
+
     {name="roboshop.dev.user.mongo",value="true",type="String"},
     {name="roboshop.dev.user.redis_host",value="redis-dev.devops86.store",type="String"},
     {name="roboshop.dev.user.mongo_url",value="mongodb://mongodb-dev.devops86.store:27017/users",type="String"}
